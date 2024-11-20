50c159cf94521b7f4c00f2600901e557fc5da553
David <162899544+Daveyify@users.noreply.github.com>
2024-11-19 20:18:12 -0500

    Update galeria.js

--- JS/galeria.js
+++ JS/galeria.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+//import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 // Configuración básica
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true; // Habilitar sombras
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
